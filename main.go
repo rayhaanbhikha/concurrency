1115e5c7ea58e1e95e43a0cb88dff16b2d053490
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-11 15:50:49 +0000

    wait group implementation

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		count("sheep")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		count("dog")
+	}()
+
+	wg.Wait()
+}
+
+func count(thing string) {
+	for i := 1; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
