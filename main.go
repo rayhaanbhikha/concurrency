c138c24df5afb9259071d8025084045dc48dccb4
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-11 20:04:09 +0000

    example of using 3 channels

--- main.go
+++ main.go
@@ -7,26 +7,56 @@ import (
 )
 
 func main() {
+	numbers := make(chan int, 100)
+	squares := make(chan int, 100)
+	errors := make(chan error, 100)
+
+	for i := 1; i <= 100; i++ {
+		numbers <- i
+	}
+	close(numbers)
+	// =============================================
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		count("sheep")
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		count("dog")
-	}()
-
+	for num := range numbers {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done()
+			result, err := square(num)
+			if err != nil {
+				errors <- err
+			}
+			squares <- result
+		}(num)
+	}
 	wg.Wait()
+
+	// =============================================
+	runSelect := true
+	for runSelect {
+		select {
+		case res := <-squares:
+			fmt.Println(res)
+		case err := <-errors:
+			fmt.Println(err)
+		default:
+			fmt.Println("finished")
+			runSelect = false
+		}
+	}
+	fmt.Println("hi")
 }
 
-func count(thing string) {
-	for i := 1; true; i++ {
-		fmt.Println(i, thing)
-		time.Sleep(time.Millisecond * 500)
+func worker(numbers, results chan int) {
+	for number := range numbers {
+		results <- number
 	}
+}
+
+func square(num int) (int, error) {
+	time.Sleep(time.Millisecond * 500)
+	if num%5 == 0 {
+		return 0, fmt.Errorf("some error %d", num)
+	}
+	return num * num, nil
 }
